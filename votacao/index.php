4c612755b5d673ebdb028e30559fed1bde572298
sonecaz <160507585+sonecaz0@users.noreply.github.com>
2024-09-09 12:24:48 -0300

    Add files via upload

---
+++ votacao/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eleição Eleitoral</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Eleição Eleitoral</h1>
+
+    <div class="conteiner_mae">
+        <div class="foto">
+        <img src="negoney.jpg" class="imagem">
+        <p class="nomes">Nego ney</p>
+        </div>
+        <div class="foto">
+        <img src="receba.jpg" class="imagem">
+        <p class="nomes">Receba</p>
+        </div>
+        <div class="foto">
+            <img src="zoio.jpg" class="imagem">
+            <p class="nomes">Olhos</p>
+        </div>
+    </div>
+    <div class="conteiner_mae">
+        <div class="foto">
+        <img src="bozo.jpg" class="imagem">
+        <p class="nomes">Bozo</p>
+        </div>
+        <div class="foto">
+        <img src="amostrado.jpg" class="imagem">
+        <p class="nomes">Amostrado</p>
+        </div>
+    </div>
+    <div class="conteiner_mae">
+    <button type="botaobranco" class="botaum">Nulo/Branco</button><br>
+    <button type="botaovoto">Mostrar Votos</button><br>
+    <button type="botaoverde" class="botao">Confirmar Voto</button>
+    </div>
+    
+</body>
+</html>
